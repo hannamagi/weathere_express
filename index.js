47b54b577ecd3a44b1a28ae00a993e14eb49c74f
Hanna <hanna.magi.001@voco.ee>
2023-02-20 10:41:26 +0200

    Created API error handling

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.get('/', function (req, res) {
-    res.render('index')
+const key = 'fdec8be240d71f7b1eeb3f48ffd910df'
+
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+                let result = {
+                    description: description,
+                    city: city,
+                    temp: temp,
+                    error: null
+                }
+                resolve(result)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+app.all('/', function (req, res) {
+    let city
+    if(req.method == 'GET') {
+        city = 'Tartu'
+    }
+    if(req.method == 'POST') {
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data => {
+            res.render('index', data)
+        })
+        .catch(error => {
+            res.render('index', {error: 'Problem with getting data, try again'})
+        })
 })
+
 app.listen(3000)
